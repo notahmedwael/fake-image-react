3a670c439bf66dc8698e355be3c9fe760c196be7
Ahmed Wael <notahmedwael888@gmail.com>
2025-07-04 10:26:16 +0300

    Initial commit with working setup

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-export const plugins = {
-    tailwindcss: {},
-    autoprefixer: {},
-};
-// This file is used to configure PostCSS, which is a tool for transforming CSS with JavaScript.
-// It includes plugins like Tailwind CSS for utility-first styling and Autoprefixer for adding vendor prefixes to CSS rules.
-// The configuration is exported as a module, which can be used by build tools like Vite or Webpack to process CSS files during the build process.
-// The plugins are specified in an object, where each key is the name of the plugin and the value is an empty object, indicating that the default configuration for each plugin should be used.
-// This setup allows for easy integration of Tailwind CSS and Autoprefixer into the project's build pipeline, enabling developers to write modern CSS with utility classes and ensuring compatibility across different browsers by automatically
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -44,13 +44,13 @@ const Home = () => {
           <button
             type="button"
             onClick={handleButtonClick}
-            className="overflow-hidden relative w-30 p-2 h-12 bg-[#43e97b] text-white border-none rounded-md text-lg font-bold cursor-pointer z-10 group"
+            className="overflow-hidden relative w-30 p-2 h-15 bg-[#43e97b] text-white border-none rounded-md text-lg font-bold cursor-pointer z-10 group"
           >
             TEST IT!
             <span className="absolute w-36 h-32 -top-8 -left-2 bg-sky-200 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-right" />
             <span className="absolute w-36 h-32 -top-8 -left-2 bg-sky-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-right" />
             <span className="absolute w-36 h-32 -top-8 -left-2 bg-sky-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-right" />
-            <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10">
+            <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-1/2 transform -translate-x-1/2 z-10">
               Explore!
             </span>
           </button>
